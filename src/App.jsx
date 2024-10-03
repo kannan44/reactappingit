efdc52a27afec3f1ddb909eb73eba602b46c1d6a
kannan44 <152164121+kannan44@users.noreply.github.com>
2024-10-03 16:27:39 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LearnComponent from "./Components/LearnComponent"
+
+function App() {
+  
+
+  return (
+    <div>Kannan</div>
+  )
+}
+
+export default App
---
+++ src/Components/LearnComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LearnComponent = () => {
+  return (
+    <div>
+        This is a text from custom component
+    </div>
+  )
+}
+
+export default LearnComponent
